daf684deb2f6f0d0890b87d6c75cdaa6a23f2703
petrynka <61549290+petrynka@users.noreply.github.com>
2025-01-28 11:53:12 +0200

    Create function for GRID
    Create function which creates CSS GRID using JS and FlexBox

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+
+function addDivsGrid(num){
+    
+    for (let i = 0; i <=(num*num)-1; i++) {
+        const content = document.createElement("div");
+        content.classList.add("block");
+        content.style.cssText = `flex: 1 1 calc(100% / ${num}); height: calc(100% / ${num});`
+        container.appendChild(content);
+    }
+
+}
+
+addDivsGrid(16);
