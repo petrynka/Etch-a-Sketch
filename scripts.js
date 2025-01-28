7c7f1202b9cd5a0f411948e3556fdc2c61c80b6b
petrynka <61549290+petrynka@users.noreply.github.com>
2025-01-28 15:10:37 +0200

    Complete whole project
    Add btn and logic for reseating grind and creating new grid

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector("#reset");
 
 
 function addDivsGrid(num){
@@ -6,19 +7,40 @@ function addDivsGrid(num){
     for (let i = 0; i <=(num*num)-1; i++) {
         const content = document.createElement("div");
         content.classList.add("block");
-        content.style.cssText = `flex: 1 1 calc(100% / ${num}); height: calc(100% / ${num});`
+        content.style.cssText = `flex: 0 0 calc(100% / ${num}); height: calc(100% / ${num});`
         container.appendChild(content);
+
+        content.addEventListener("mouseover", () =>{
+            content.style.backgroundColor = "black"
+        })
+
+        // const blocks = document.querySelectorAll(".block");
+
+        // blocks.forEach(block => {
+        //     block.addEventListener("mouseover", () => {
+        //     block.style.backgroundColor = "black"
+        //     });
+        // });
     }
 
 }
 
 addDivsGrid(16);
 
-const blocks = document.querySelectorAll(".block");
+function gridCleaner(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
 
-blocks.forEach(block => {
-    block.addEventListener("mouseenter", () => {
-        block.style.backgroundColor = "black"
-    });
+resetBtn.addEventListener("click", () =>{
+    let enteredNum = prompt("Please enter number from 1 to 100", );
+    if(+enteredNum<1 || +enteredNum>100){
+        alert("Please enter number between 1 and 100");
+        return;
+    }
+    gridCleaner();
+    
+    addDivsGrid(+enteredNum);
+    
 });
-
